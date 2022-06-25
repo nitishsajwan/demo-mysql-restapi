77801a370a32909c1d2fe33cb30e0fa2c45f56dc
nitishs <nitish.sajwan@ukg.com>
2022-06-25 13:22:40 +0530

    demo-mysql first commit

--- src/main/java/gfg/jdbl/demomysql/repository/PersonRepository.java
+++ src/main/java/gfg/jdbl/demomysql/repository/PersonRepository.java
@@ -1,13 +1,32 @@
 package gfg.jdbl.demomysql.repository;
 
 import gfg.jdbl.demomysql.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
 
 @Repository
 public class PersonRepository {
 
+    private static final Logger logger= LoggerFactory.getLogger(PersonRepository.class);
+
+    @Autowired
+    Connection connection;
+
+    public PersonRepository(Connection connection) {
+        this.connection = connection;
+        logger.info("Connection object {}",connection);
+
+    }
+
+
     public void insertPerson(Person person){
     // method can be used for pushing data into the database
+
+
     }
 
 }
